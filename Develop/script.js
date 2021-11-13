24419eda5bff7ed326c77ad3eab8c8e0884b7e4d
Prince of Zamunda <omarxsawyer@gmail.com>
2021-11-13 13:08:57 -0700

    defining variables

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,48 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+/ declare variables for each element
+
+var password = document.getElementById("password");
+
+var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*_-=+";
+var passwordLength = 12;
+var password = "";
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//
+
+for (var i = 0; i <= passwordLength; i++) {
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+}
+*/
